93da89cf73c66b5baed0b1eb4a9cd8ad90fb0c66
gabriellocp <gabriel_campista@hotmail.com>
2026-05-09 17:58:05 -0300

    chore: Config App fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
+import {
+  HostGrotesk_400Regular,
+  HostGrotesk_500Medium, HostGrotesk_600SemiBold, HostGrotesk_700Bold
+} from '@expo-google-fonts/host-grotesk';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import "./styles/global.css";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [loaded, error] = useFonts({
+    HostGrotesk_400Regular, HostGrotesk_500Medium, HostGrotesk_600SemiBold, HostGrotesk_700Bold
+  });
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text className='font-sans-semibold'>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
