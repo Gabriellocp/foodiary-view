72a7f24b629683cdd16073c077193aff9feeafb5
gabriellocp <gabriel_campista@hotmail.com>
2026-05-09 19:00:19 -0300

    fix: Moved Tailwind config to extended one

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,15 @@ module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
-    fontFamily: {
-      'sans-regular': ['HostGrotesk_400Regular'],
-      'sans-medium': ['HostGrotesk_500Medium'],
-      'sans-semibold': ['HostGrotesk_600SemiBold'],
-      'sans-bold': ['HostGrotesk_700Bold'],
-    },
-    colors: colors
+    extend: {
+      fontFamily: {
+        'sans-regular': ['HostGrotesk_400Regular'],
+        'sans-medium': ['HostGrotesk_500Medium'],
+        'sans-semibold': ['HostGrotesk_600SemiBold'],
+        'sans-bold': ['HostGrotesk_700Bold'],
+      },
+      colors: colors
+    }
   },
   plugins: [],
 }
