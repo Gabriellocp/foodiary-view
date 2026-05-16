c9d251df978d93c0a934ec4d83aea005ca3b77e7
gabriellocp <gabriel_campista@hotmail.com>
2026-05-16 17:15:36 -0300

    feat: Add AudioModal component

--- src/components/CreateMealBottomBar.tsx
+++ src/components/CreateMealBottomBar.tsx
@@ -1,10 +1,13 @@
 import { CameraIcon, MicIcon } from "lucide-react-native";
+import { useState } from "react";
 import { View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { AudioModal } from "./AudioModal";
 import { Button } from "./Button";
 
 export function CreateMealBottomBar() {
-    const { bottom } = useSafeAreaInsets()
+    const { bottom } = useSafeAreaInsets();
+    const [isAudioModalOpen, setIsAudioModalOpen] = useState(false);
     return (
         <View
             className="
@@ -13,12 +16,13 @@ export function CreateMealBottomBar() {
             "
             style={{ height: 80 + bottom }}
         >
-            <Button size="icon" color="gray">
+            <Button size="icon" color="gray" onPress={() => setIsAudioModalOpen(true)}>
                 <MicIcon />
             </Button>
             <Button size="icon" color="gray">
                 <CameraIcon />
             </Button>
+            <AudioModal open={isAudioModalOpen} onClose={() => setIsAudioModalOpen(false)} />
         </View>
     )
 }
