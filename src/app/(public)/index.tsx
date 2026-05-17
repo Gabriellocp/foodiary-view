01e5ba40e8bd24012ac1836f997e252a2d0ae695
gabriellocp <gabriel_campista@hotmail.com>
2026-05-17 18:48:06 -0300

    feat: Configure GreetingsPage

--- src/app/(public)/index.tsx
+++ src/app/(public)/index.tsx
@@ -1,9 +1,37 @@
-import { Text, View } from "react-native";
-
+const OnboardingBackground = require('@/assets/onboarding-bg/onboarding-bg.png');
+import { Button } from "@/components/Button";
+import { Logo } from "@/components/Logo";
+import { Link } from "expo-router";
+import { ImageBackground, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function GreetingsPage() {
     return (
-        <View>
-            <Text>Entrar no APP</Text>
-        </View>
+        <ImageBackground
+            source={OnboardingBackground}
+            className="flex-1"
+        >
+            <SafeAreaView className="flex-1">
+                <View className="flex-1 items-center justify-between">
+
+                    <View className="mx-auto mt-4">
+                        <Logo width={100} height={32} />
+                    </View>
+                    <View className="w-full items-center">
+                        <Text className="font-sans-semibold text-[32px] text-white w-[311px] text-center">Controle sua dieta de forma simples!</Text>
+                        <View className="mx-5 w-full p-5 mt-6">
+                            <Link href={'/(public)/signup'} asChild>
+                                <Button className="w-full">Criar conta?</Button>
+                            </Link>
+                        </View>
+                        <View className="mt-[30px] flex-row items-center gap-2">
+                            <Text className="text-white font-sans-regular text-base">Já tem uma conta? </Text>
+                            <Link href={'/(public)/signin'}>
+                                <Text className="text-lime-500 font-sans-medium text-base">Acesse agora!</Text>
+                            </Link>
+                        </View>
+                    </View>
+                </View>
+            </SafeAreaView>
+        </ImageBackground>
     )
 }
